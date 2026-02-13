dec4777676578200705b9f74c212ba3d8f2ac90a
Anthony Sciola <asciola@gmail.com>
2026-02-12 21:40:07 -0500

    Fix mixedprecision

--- src/layers/positional.py
+++ src/layers/positional.py
@@ -5,6 +5,9 @@ from tensorflow.keras.layers import Layer
 
 def get_angles_prior_knowledge(times, d_model):
     with tf.name_scope("Get_Angles") as scope:
+        # Cast to float32 for computation
+        times_f32 = tf.cast(times, tf.float32)
+
         t_init = tf.math.log(3/24)  # 3 hours in days
         t_end = tf.math.log(3.*365) # 3 years in days
         # Sample in log space
@@ -14,24 +17,26 @@ def get_angles_prior_knowledge(times, d_model):
         # Compute the angular frequency
         angle_rates = 2*3.14159*angle_rates
         # Compute the argument of the trig functions
-        angle_rates = times * angle_rates
+        angle_rates = times_f32 * angle_rates
         return angle_rates
 
 @tf.function
 def get_angles_astromer(times, d_model, c=2., base=10000):
     with tf.name_scope("Get_Angles") as scope:
+        times_f32 = tf.cast(times, tf.float32)
         dim_indices = tf.range(d_model, dtype=tf.float32)
-        
         exponent = tf.divide(tf.multiply(c, dim_indices),
                              tf.cast(d_model, tf.float32))
-        
         angle_rates = tf.pow(tf.cast(base, dtype=tf.float32), exponent)
         angle_rates = tf.math.reciprocal(angle_rates)
-        angle_rates = times * angle_rates
+        angle_rates = times_f32 * angle_rates
         return angle_rates
 
 def positional_encoding(times, d_model, base=10000, mjd=False, c=2.):
     with tf.name_scope("PosEncoding") as scope:
+        # Store original dtype to cast back at the end
+        original_dtype = times.dtype
+
         if mjd:
             indices = times
         else:
@@ -53,7 +58,7 @@ def positional_encoding(times, d_model, base=10000, mjd=False, c=2.):
         indices = tf.range(0, tf.shape(x_transpose)[0])
         x_transpose = tf.map_fn(lambda x: fn(x),  (x_transpose, indices))[0]
         pos_encoding = tf.transpose(x_transpose, [2, 1, 0])
-        return tf.cast(pos_encoding, dtype=tf.float32)
+        return tf.cast(pos_encoding, dtype=original_dtype)
 
 class PositionalEncoder2(tf.keras.layers.Layer):
     def call(self, times, d_model, base=10000, mjd=False, c=2.):
